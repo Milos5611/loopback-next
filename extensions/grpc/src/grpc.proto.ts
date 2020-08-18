5b91a8ffa509ee417e225e470c478b7dd35a48a4
Raymond Feng <enjoyjava@gmail.com>
2020-08-18 16:39:58 -0700

    feat(grpc): switch to @grpc/* packages

--- extensions/grpc/src/grpc.proto.ts
+++ extensions/grpc/src/grpc.proto.ts
@@ -3,14 +3,14 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+import {GrpcObject, MethodDefinition, ServiceDefinition} from '@grpc/grpc-js';
 import {ContextView, filterByTag, inject} from '@loopback/core';
 import {execSync} from 'child_process';
 import debugFactory from 'debug';
-import {GrpcObject, MethodDefinition, ServiceDefinition} from 'grpc';
 import path from 'path';
 import {GrpcBindings, GrpcTags} from './keys';
 import {GrpcOperation, GrpcServerConfig} from './types';
-const debug = debugFactory('loopback:grpc:generator');
+const debug = debugFactory('loopback:grpc:proto-manager');
 
 /**
  * GRPC TypeScript generator.
@@ -65,8 +65,10 @@ export class ProtoManager {
         for (const key in pkgObj) {
           const child = pkgObj[key] as GrpcObject;
           if (child.service) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const service = child.service as ServiceDefinition<any>;
+            const service = (child.service as unknown) as ServiceDefinition<
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              any
+            >;
             debug('Protobuf service: %s', key, service);
             for (const m in service) {
               const method = service[m] as MethodDefinition<unknown, unknown>;
@@ -125,7 +127,8 @@ export class ProtoManager {
       ),
     );
     return execSync(
-      `${protocPath} --plugin=protoc-gen-ts=${pluginPath} --ts_out service=true:${root} -I ${root} ${proto}`,
+      `${protocPath} --plugin=protoc-gen-ts=${pluginPath}
+ --ts_out=generate_package_definition:service=true:${root} -I ${root} ${proto}`,
     );
   }
 }
